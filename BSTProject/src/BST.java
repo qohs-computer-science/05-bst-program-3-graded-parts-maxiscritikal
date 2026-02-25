bc6a6d3618a29236b7dc65d29005732efd960b0f
maxiscritikal <101420657+maxiscritikal@users.noreply.github.com>
2026-02-25 18:38:44 +0000

    first commit

--- BSTProject/src/BST.java
+++ BSTProject/src/BST.java
@@ -1,6 +1,48 @@
+/*
+ * Max Korsa 
+ * 2/25/26
+ * PD: 6
+ * Creates a BST, with methods for everything you need, that is super epic.
+ */
 import java.lang.Comparable;
-
 public class BST implements BSTInterface
 {
+    private TreeNode root;
 
+	public BST()
+	{
+		root = null;
+	}
+
+    public void add(Object obj)
+    {
+        if(root == null)
+		{
+			root = new TreeNode(obj);
+		}
+
+		else if(root.getValue().compareTo(obj) >= 0)
+		{
+			if(root.getLeft() != null)
+			{
+				obj = root.getLeft();
+			}
+			else
+			{
+				root.setLeft(new TreeNode(obj));
+			}
+		}
+
+		else if(root.getValue().compareTo(obj) < 0)
+		{
+			if(root.getRight() != null)
+			{
+				obj = root.getRight();
+			}
+			else
+			{
+				root.setRight(new TreeNode(obj));
+			}
+		}
+    }
 }
--- BSTProject/src/BSTTester.java
+++ BSTProject/src/BSTTester.java
@@ -1,14 +1,14 @@
 /*
- * TODO: Name
- * TODO: Date
- * TODO: Class Period
- * TODO: Program Description
+ * Max Korsa 
+ * 2/25/26
+ * PD: 6
+ * Creates a BST Tester File With Many Scenarios To Check Your Epic Program.
  */
-
  import java.lang.Comparable;
+ public class BSTTester 
+{
+   public static void main(String[] args) 
+  {
 
- public class BSTTester {
-   public static void main(String[] args) {
-   
-	
- }
+  }
+}
